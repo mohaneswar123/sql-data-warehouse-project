50d7631deee75445d7ebe3c4b4e29b2faf251f25
mohaneswar123 <163732824+mohaneswar123@users.noreply.github.com>
2026-04-11 07:19:35 +0530

    Enhance init_database.sql with comments and warnings
    Added a detailed comment block explaining the script's purpose and warnings regarding database deletion.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,5 +1,16 @@
 --Create DataBase and Schema
 
+/*
+Script Purpose : 
+	1. Create DataBase "DataWarehouseProject" for the project if already exists it will be droped will be
+	created as new one .
+	2. Create Schema for every layer for "Bronze", "Silver" and "Gold" in the DataBase
+
+
+WARNING :	 
+	1. By Running this script "DataWarehouseProject" database will be permanently deleted and exising data and schemas
+	aslo droped so make sure before running this script having backup of database
+*/
 
 -- switch to master before creating DB
 use master
